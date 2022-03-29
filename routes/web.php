5781685dc721c3407a1b26afe2e0c2d8c84be200
mazharul95 <mipiyas07@gmail.com>
2022-03-29 20:09:27 +0600

    Database - Eloquent One to One Relationship CRUD

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Address;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/insert', function (){
+    $user = User::findOrFail(1);
+
+    $address = new Address(['name'=>'3333 nayapara madhabpur hobigonj Sylhet']);
+
+    $user->address()->save($address);
+});
+
+Route::get('/update', function(){
+
+   $address = Address::whereUserId(1)->first();
+   $address->name = "Coxbazaar chittagong,Bangladesh";
+
+   $address->save();
+});
+
+Route::get('/read', function(){
+   $user = User::findOrFail(1);
+
+   echo $user->address->name;
+});
+
+Route::get('/delete', function(){
+    $user = User::findOrFail(1);
+
+    $user->address()->delete();
+
+    return "done";
+});
